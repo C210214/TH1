9106185f64e0c699bb1832fd048436505cfa7e98
c210214 <92442535+C210214@users.noreply.github.com>
2023-09-20 14:22:33 +0700

    first coit

--- src/TH2.java
+++ src/TH2.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TH2 {
+public class TH2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 10;
+        float f = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.printf(" i = %d\n f = %.1f\n b = %b\n c = %c\n s = %s\n", i, f, b, c, s);
+    }
 }
--- src/TH3.java
+++ src/TH3.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TH3 {
+import java.util.Scanner;
+
+public class TH3 {
+    public static void main(String[] args) {
+        float width, height;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        width = sc.nextFloat();
+        System.out.print("Enter height: ");
+        height = sc.nextFloat();
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
 }
